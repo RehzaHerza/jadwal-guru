af0b4efedddfefe2108debcb3d85019999a3e2a5
RehzaHerza <hago.hg5434@gmail.com>
2026-02-05 10:31:16 +0700

    Fix service worker update dan manifest PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jadwal-guru-v2";
+const CACHE_NAME = "jadwal-guru-v3";
 
 const urls = [
 "/",
@@ -12,12 +12,28 @@ const urls = [
 
 self.addEventListener("install",e=>{
 e.waitUntil(
-caches.open(CACHE_NAME).then(c=>c.addAll(urls))
+caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE))
 );
+self.skipwaiting();
 });
 
-self.addEventListener("fetch",e=>{
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e=>{
 e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
+caches.match(e.request).then(res => res || fetch(e.request))
 );
 });
